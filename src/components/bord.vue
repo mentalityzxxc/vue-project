a70568433142ea190a97193efb74d60797cf2c37
mentalityzxxc <egkost07@gmail.com>
2023-07-14 21:26:29 +0300

    les 24

--- src/components/bord.vue
+++ src/components/bord.vue
@@ -5,24 +5,125 @@
             return{
             hello:"privet",
             wallpapers:[
-                   'https://i.pinimg.com/originals/b3/cf/82/b3cf8221bf35baf3d4faa68473811fc9.jpg',
-                    'https://www.passion.ru/thumb/1500x0/filters:quality(75):no_upscale()/imgs/2022/03/24/16/5323877/30c1e09df2ea3734f0635eb503b486865d338c36.jpg'
-            ]
+                "https://i.pinimg.com/originals/b3/cf/82/b3cf8221bf35baf3d4faa68473811fc9.jpg",
+                "https://www.passion.ru/thumb/1500x0/filters:quality(75):no_upscale()/imgs/2022/03/24/16/5323877/30c1e09df2ea3734f0635eb503b486865d338c36.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/BlackWhite12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/4.png",
+                "https://klevtsovaelena.github.io/wallpaper/img/5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Shine12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Winter12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire3.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire7.jpeg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire8.png",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Fire12.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower1.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower2.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower3.jpeg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower4.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower5.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower6.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower7.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower8.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower9.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower10.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower11.jpg",
+                "https://klevtsovaelena.github.io/wallpaper/img/Flower12.jpg",
+            ],
+            currentWallpaper: 0,
+            activeMenu: false,
             };
         },
-        props: ['test']
+        props: ['test'],
+        methods: {
+          changeBackground(number){
+            this.currentWallpaper = number
+            this.activeMenu = false;
+          },
+          openCloseMenu(){
+            //alert('функция отроботона')
+            //this.activeMenu = true;
+            if (this.activeMenu) {
+              this.activeMenu = false;
+            } else {
+              this.activeMenu = true;
+            }
+          }
+        }
     }
 </script>
 
 <template>
-  <div>
-    {{ hello }}
-    {{ test }}
-    <li v-for="item in wallpapers">
-    {{ item }}
-    <img :src="item">
-    </li>
+<div>
+  <div class='board' :style="{'background-Image':`url('${wallpapers[currentWallpaper]}')`}">
+      <button class='board__button-change-theme' @click='openCloseMenu'>
+        <img :src='wallpapers[currentWallpaper]' />Сменить фон
+      </button>
+    <div :class="[activeMenu ? 'board__theme-wrapper board__theme-wrapper--active' : 'board__theme-wrapper']">
+        <div id="wallpapers">
+          <div v-for="(item, i) in wallpapers"
+            class='wallpapers__item'
+            :key="index"
+            :style="{'background-Image':`url('${item}')`}"
+            @click='changeBackground(i)'>
+          </div>
+        </div>
+      <div class='close-wallpapers' @click='openCloseMenu'>
+      {{'<'}}
+    </div>
   </div>
+    </div>
+</div>
 </template>
 
 <style scoped>
@@ -44,6 +145,7 @@
       width: 100%;
       left: 0;
       top: 0;
+      height: 100%;
   }
   .wallpapers__item {
       display: inline-block;
@@ -88,7 +190,7 @@
     display: flex;
     position: absolute;
     top: 0;
-    left: 0;
+    right: 0;
   }
   .board__button-change-theme img{
     max-width: 50px;
